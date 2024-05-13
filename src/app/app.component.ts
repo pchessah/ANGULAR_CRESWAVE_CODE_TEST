35f05377cdbd08912ae50e1eb73e2f84ce4b2f29
Paul Chesa <paulchesa1@gmail.com>
2024-05-13 05:53:10 +0300

    feat: Finish up setup of project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoadingComponent } from './components/loading/loading.component';
+import { LoadingService } from './shared/services/loading/loading.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoadingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'ANGULAR_CRESWAVE_CODE_TEST';
+  loadingService = inject(LoadingService)
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'landing',
+    loadComponent: () => import('./pages/landing/landing.component').then((c) => c.LandingComponent)
+  },
+  {
+    path: 'task/:id',
+    loadComponent: () => import('./pages/tasks-details/tasks-details.component').then((c) => c.TasksDetailsComponent)
+  },
+  {
+    path: '',
+    redirectTo: '/landing',
+    pathMatch: 'full'
+  }
+];
