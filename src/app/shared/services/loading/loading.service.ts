8b337fbcef09aa4e086352f3562c51aee8a00fa4
Paul Chesa <paulchesa1@gmail.com>
2024-05-13 05:52:11 +0300

    feat: Added service to simulate loading

---
+++ src/app/shared/services/loading/loading.service.ts
@@ -0,0 +1,8 @@
+import { Injectable, signal, WritableSignal } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoadingService {
+  isLoading :WritableSignal<boolean> = signal(false);
+}
